919f1a9efd5e13092eddae3d635235dc0c04a7f1
David Searl <dhsearl@gmail.com>
2019-12-02 10:22:56 -0600

    added two minute mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 const Header = () => (
-  <div className="app-header-block"> 
-     <h1 className="app-header">RANKCHOICE</h1>
-     <span className="app-suffix">.io</span>
-  </div>
+    <> <div className="app-header-block"> 
+    <h1 className="app-header">RANKCHOICE</h1>
+    <span className="app-suffix">.io</span>
+ </div>
+ <div className="header-minutes"><h3>In TWO Minutes</h3></div></>
 );
 
 export default Header;
